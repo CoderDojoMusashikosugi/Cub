7b62094d46237e78556608613072007a4c0d9e8f
tkg-hiroshi <hrshtkg@gmail.com>
2023-07-05 16:59:20 +0900

    add: rtk start command

---
+++ system/ros/ws/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+RTK_BASE_STATION=ntrip1.bizstation.jp
+RTK_SENSER_ADDRESS=ttyACM0
+
+cd /cub/colcon
+colcon build
+source install/setup.bash
+ros2 launch ublox_gps ublox_gps_node-launch.py &
+
+cd /cub/RTKLIB/lib/iers/gcc
+make
+cd /cub/RTKLIB/app/consapp
+make
+cd str2str/gcc
+./str2str -in ntrip://$RTK_BASE_STATION -out serial://$RTK_SENSER_ADDRESS
